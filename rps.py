a03aaade238e51467f2918dc6cc9d637023ec397
Lewis Munyi <lewismunyi97@gmail.com>
2018-05-08 12:09:34 +0300

    Improve program
    This game would be much more fun if played between a user and their computer
    Added some (few :p) to make it playable between the user and their computer. The points award bit is buggy, but that's easy to fix.

--- rps.py
+++ rps.py
@@ -1,30 +1,67 @@
+#Import required modues
 import sys
+import getpass
+import random
+import time
 
-user1 = input("What's your name?")
-user2 = input("And your name?")
-user1_answer = input("%s, do yo want to choose rock, paper or scissors?" % user1)
-user2_answer = input("%s, do you want to choose rock, paper or scissors?" % user2)
 
-def compare(u1, u2):
-    if u1 == u2:
+#Find winner and add points
+def compare(human_ans, comp_ans, human_points, computer_points):
+    if human_ans is comp_ans:
         return("It's a tie!")
-    elif u1 == 'rock':
-        if u2 == 'scissors':
-            return("%s Wins!!!" % user1)
-        else:
-            return("%s Wins!!!" %user2)
-    elif u1 == 'scissors':
-        if u2 == 'paper':
-            return("%s Wins!!!"  %user1)
-        else:
-            return("%s wins!!!" %user2)
-    elif u1 == 'paper':
-        if u2 == 'rock':
-            return("%s wins!!!" %user1)
-        else:
-            return("%s wins!!!" %user2)
-    else:
-        return("Invalid input! You have not entered rock, paper or scissors, try again.")
-        sys.exit()
 
-print(compare(user1_answer, user2_answer))
+    elif human_ans is 1 and comp_ans is 2:
+        computer_points += 1
+        return ("%s (Paper) beats %s (Rock)" %(str(computer), str(human)))
+
+    elif human_ans is 1 and comp_ans is 3:
+        human_points += 1
+        return ("%s (Rock) beats %s (Scissors)" %(str(human), str(computer)))
+
+    elif human_ans is 2 and comp_ans is 1:
+        human_points += 1
+        return ("%s (Paper) beats %s  (Rock)" %(str(human), str(computer)))
+
+    elif human_ans is 2 and comp_ans is 3:
+        computer_points += 1
+        return ("%s (Scissors) beats %s  (Paper)" %(str(computer), str(human)))
+
+    elif human_ans is 3 and comp_ans is 1:
+        human_points += 1
+        return ("%s (Rock) beats %s (Scissors)" %(str(human), str(computer)))
+
+    elif human_ans is 3 and comp_ans is 2:
+        human_points += 1
+        return ("%s (Scissors) beats %s  (Paper)" %(str(human), str(computer)))
+    else:
+        return ("Wrong input!")
+
+
+def play():
+    computer_answer = random.randint(1,3) #Computer makes move
+    time.sleep(1) #Sleep to simulate the human time taken to come up with a move
+    choices = "Your turn...\n\t[1] Rock\n\t[2] Paper \n\t[3] Scissors\n"
+    human_answer = input(choices)
+    print ("My move was " + str(computer_answer))
+    print(compare (int(human_answer), int(computer_answer), int(human_points), int(computer_points)))
+    return 0
+
+
+
+
+#################### Start program ###################
+if __name__ == "__main__":
+    #Get computer name, player's name and number of game passes    
+    human_points = 0    
+    computer_points = 0
+    computer = getpass.getuser()
+    human = input("Hello there human. My name is " + computer + ". \nWhat's yours? \n")
+    moves = input ("Alrighty " + human + ". Let's see if the brain really is smarter than the processor :P\n\nFirst enter the number moves we'll play before tallying the results:\n" )
+    for i in range (int(moves)):
+        play()
+    if human_points > computer_points:
+        print("%s wins with %i points against %s's %i points. " %(str(human), int(human_points), str(computer), int(computer_points)))
+    else:
+        print("%s wins with %i points against %s's %i points. " %(str(computer), int(computer_points,), str(human), int(human_points)))
+
+    sys.exit() #Exit
